20c5e9bc72369482aadf02f7aba9a05c999f9e5d
Dawid <125745455+xenorre@users.noreply.github.com>
2025-10-22 23:20:59 +0200

    feat: add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "../components/sections/Hero";
+
 export default function Home() {
-  return <div>Hello, World!</div>;
+  return (
+    <div className="min-h-screen bg-linear-to-br from-gray-50 to-gray-100">
+      <Hero />
+    </div>
+  );
 }
